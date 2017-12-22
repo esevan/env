55d9ac2b6972f4aebaae6d52074064dc4c9a9132
Eunsoo Park <esevan.park@samsung.com>
2017-12-22 19:10:49 +0900

    Update esevan_confg.sh

--- esevan_confg.sh
+++ esevan_confg.sh
@@ -2,9 +2,7 @@
 echo "Importing bashrc & virmc..."
 cd ~/
 git clone --quiet https://github.com/esevan/env.git
-cp env/.vimrc ~/.vimrc
-cp env/.bashrc ~/.bashrc
-cp env/.inputrc ~/.inputrc
-cp env/.tmux.conf ~/.tmux.conf
+mv env/* ~/
+rmdir env
 echo "Installation complete!"
 
